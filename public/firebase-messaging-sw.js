5e363d0a03ede12bd62625b897bbbdde7a1da876
JustGTWR25 <greg.t.watts@outlook.com>
2026-04-13 15:45:44 -0500

    Create firebase-messaging-sw.js

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+  apiKey:            "AIzaSyD6GE7dsUhXUtQR4faVpIVotctG1lS003Y",
+  authDomain:        "watts-f5205.firebaseapp.com",
+  databaseURL:       "https://watts-f5205-default-rtdb.firebaseio.com",
+  projectId:         "watts-f5205",
+  storageBucket:     "watts-f5205.firebasestorage.app",
+  messagingSenderId: "982199951741",
+  appId:             "1:982199951741:web:a89cebda2dff026f7eac14",
+});
